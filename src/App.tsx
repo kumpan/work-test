473cefb3a5d51248e4b40818f4a1c6c33dfaa2fa
Anton Classon <anton@kumpan.se>
2022-01-11 16:22:28 +0100

    Setting up structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
-import pokemonApi from "./domain/api";
+import List from "./components/List";
 
 const App = () => {
-	console.log("App");
-
-	const first = pokemonApi.useGetPokemonListQuery("");
-
-	console.log(first.data?.results);
-
-	const second = pokemonApi.useGetPokemonListQuery(first.data?.next || "");
-
-	console.log(second.data?.results);
-
-	return (
-		<div>
-			<h2>App</h2>
-		</div>
-	);
+	return <List />;
 };
 
 export default App;
