041831bbf3d2b6d95224a519aa4a9205da4ea0b8
Anton Classon <anton@kumpan.se>
2022-01-12 14:50:55 +0100

    Removing unused

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { Global, css } from "@emotion/react";
 import { Provider } from "react-redux";
 import store from "./domain/store";
-import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 
 const globalStyle = css`
@@ -40,8 +39,3 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById("root"),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
