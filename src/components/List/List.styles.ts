3d64a021010f225335e3228bc164bb70d59e82a8
Anton Classon <anton@kumpan.se>
2022-01-12 14:51:03 +0100

    Style

--- src/components/List/List.styles.ts
+++ src/components/List/List.styles.ts
@@ -1,14 +1,20 @@
 import styled from "@emotion/styled";
+import { mediaQuery, Breakpoints } from "../../domain/mediaQuery";
 
 export const MainList = styled.div`
 	display: flex;
 	flex-direction: column;
-	max-width: 600px;
-	width: 100%;
-	margin-top: 36px;
+	width: calc(100% - (2 * 16px));
+	margin: 32px 0 0;
 	padding: 16px;
 	font-size: 32px;
 	background: #fff;
-	border-radius: 2px;
-	box-shadow: 0 4px 32px #aab;
+	border-radius: 4px;
+	box-shadow: 0 8px 32px #aab;
+
+	${mediaQuery(Breakpoints.md)} {
+		max-width: 600px;
+		width: 100%;
+		margin-top: 36px;
+	}
 `;
